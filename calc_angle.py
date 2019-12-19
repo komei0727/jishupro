bcbab5f1d37caf8457826aaf9e21fb447909e98d
komei_takabayashi <krymei2730@gmail.com>
2019-12-19 17:48:34 +0900

    文字の追加

--- calc_angle.py
+++ calc_angle.py
@@ -5,9 +5,9 @@ from module.get_coordinate import mouseParam
 from module.calc_coordinate import calc_coordinate
 from module.inverse_kinematics import inverse_kinematics
 
-image = "image/お.jpg"
-original_data = "data/お_original_2.csv"
-data = "data/お_2.csv"
+image = "image/ぐ.jpg"
+original_data = "data/ぐ_original_3.csv"
+data = "data/ぐ_3.csv"
 #angle_data = "angle_data/あ_2.csv"
 
 if __name__ == "__main__":
