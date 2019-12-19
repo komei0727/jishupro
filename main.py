b08896c0c855bf8ee45530d01c3eb2d0027c94c0
komei_takabayashi <krymei2730@gmail.com>
2019-12-19 13:50:44 +0900

    文字データの向きを修正

--- main.py
+++ main.py
@@ -4,40 +4,40 @@ import time
 import csv
 from PCA9685 import PCA9685
 
-if __name__=='__main__':
+if __name__ == '__main__':
 
-  pwm = PCA9685(0x40, debug=False)
-  pwm.setPWMFreq(60)
+    pwm = PCA9685(0x40, debug=False)
+    pwm.setPWMFreq(60)
 
-  with open('angle_data/a_1.csv') as f:
-      reader = csv.reader(f)
-      angles = [row for row in reader]
+    with open('angle_data/a_1.csv') as f:
+        reader = csv.reader(f)
+        angles = [row for row in reader]
 
-  for i in range(len(angles)):
-      pulse1 = pwm.ConvertPulse(float(angles[i][0]))
-      pulse2 = pwm.ConvertPulse(float(angles[i][1]))
-      pwm.setServoPulse(0, pulse1)
-      pwm.setServoPulse(1, pulse2)
-      time.sleep(0.001)
+    for i in range(len(angles)):
+        pulse1 = pwm.ConvertPulse(float(angles[i][0]))
+        pulse2 = pwm.ConvertPulse(float(angles[i][1]))
+        pwm.setServoPulse(0, pulse1)
+        pwm.setServoPulse(1, pulse2)
+        time.sleep(0.001)
 
-  with open('angle_data/a_2.csv') as f:
-      reader = csv.reader(f)
-      angles = [row for row in reader]
+    with open('angle_data/a_2.csv') as f:
+        reader = csv.reader(f)
+        angles = [row for row in reader]
 
-  for i in range(len(angles)):
-      pulse1 = pwm.ConvertPulse(float(angles[i][0]))
-      pulse2 = pwm.ConvertPulse(float(angles[i][1]))
-      pwm.setServoPulse(0, pulse1)
-      pwm.setServoPulse(1, pulse2)
-      time.sleep(0.001)
+    for i in range(len(angles)):
+        pulse1 = pwm.ConvertPulse(float(angles[i][0]))
+        pulse2 = pwm.ConvertPulse(float(angles[i][1]))
+        pwm.setServoPulse(0, pulse1)
+        pwm.setServoPulse(1, pulse2)
+        time.sleep(0.001)
 
-  with open('angle_data/a_3.csv') as f:
-      reader = csv.reader(f)
-      angles = [row for row in reader]
+    with open('angle_data/a_3.csv') as f:
+        reader = csv.reader(f)
+        angles = [row for row in reader]
 
-  for i in range(len(angles)):
-      pulse1 = pwm.ConvertPulse(float(angles[i][0]))
-      pulse2 = pwm.ConvertPulse(float(angles[i][1]))
-      pwm.setServoPulse(0, pulse1)
-      pwm.setServoPulse(1, pulse2)
-      time.sleep(0.001)
+    for i in range(len(angles)):
+        pulse1 = pwm.ConvertPulse(float(angles[i][0]))
+        pulse2 = pwm.ConvertPulse(float(angles[i][1]))
+        pwm.setServoPulse(0, pulse1)
+        pwm.setServoPulse(1, pulse2)
+        time.sleep(0.001)
